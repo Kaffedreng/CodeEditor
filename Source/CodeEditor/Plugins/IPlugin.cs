58183dbcfa21b4e367ed52104b6710bf8969f390
Christian Mitteldorf <christian.km@me.com>
2015-11-12 12:52:35 +0100

    Added IPlugin and PluginLoader

---
+++ Source/CodeEditor/Plugins/IPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEditor.Plugins {
+
+    public interface IPlugin {
+
+        string Name { get; }
+
+        void Do();
+    }
+}
---
+++ Source/CodeEditor/Plugins/PluginLoader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEditor.Plugins {
+
+    /// <summary>
+    /// The PluginLoader class handles loading of all Plugins.
+    /// 
+    /// A plugin must be a .dll file.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class PluginLoader<T> {
+
+        /// <summary>
+        /// Load all plugins in the specified path.
+        /// </summary>
+        /// <param name="path">The path where the plugins reside.</param>
+        /// <returns></returns>
+        public static ICollection<T> LoadPlugins(string path) {
+            string[] dllFileNames = null;
+
+            if (Directory.Exists(path)) {
+                dllFileNames = Directory.GetFiles(path, "*.dll");
+
+                ICollection<Assembly> assemblies = new List<Assembly>(dllFileNames.Length);
+                foreach (string dllFile in dllFileNames) {
+                    AssemblyName an = AssemblyName.GetAssemblyName(dllFile);
+                    Assembly assembly = Assembly.Load(an);
+                    assemblies.Add(assembly);
+                }
+
+                Type pluginType = typeof(T);
+                ICollection<Type> pluginTypes = new List<Type>();
+                foreach (Assembly assembly in assemblies) {
+                    if (assembly != null) {
+                        Type[] types = assembly.GetTypes();
+
+                        foreach (Type type in types) {
+                            if (type.IsInterface || type.IsAbstract) {
+                                continue;
+                            }
+                            else {
+                                if (type.GetInterface(pluginType.FullName) != null) {
+                                    pluginTypes.Add(type);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                ICollection<T> plugins = new List<T>(pluginTypes.Count);
+                foreach (Type type in pluginTypes) {
+                    T plugin = (T)Activator.CreateInstance(type);
+                    plugins.Add(plugin);
+                }
+
+                return plugins;
+            }
+
+            return null;
+        }
+    }
+
+}
