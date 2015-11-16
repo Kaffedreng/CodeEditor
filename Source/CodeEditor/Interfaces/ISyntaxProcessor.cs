158c25043372883be95a75aa7b3edfd2fb593ff7
Christian Mitteldorf <christian.km@me.com>
2015-11-16 10:27:33 +0100

    Added ISyntaxProcessor

---
+++ Source/CodeEditor/Interfaces/ISyntaxProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Documents;
+
+namespace CodeEditor.Interfaces {
+
+    public interface ISyntaxProcessor {
+
+        Regex SplitWordsRegex { get; }
+        int GetWordTypeId(string word);
+        Inline FormatInlineForId(Inline inline, int id);
+    }
+}
