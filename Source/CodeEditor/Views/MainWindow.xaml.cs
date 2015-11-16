607a227378494284bbfae2bfca54eabb8842ad15
Christian Mitteldorf <christian.km@me.com>
2015-11-16 10:52:52 +0100

    Added MainWindowViewModel
    Connected XAML to ViewModel

--- Source/CodeEditor/Views/MainWindow.xaml.cs
+++ Source/CodeEditor/Views/MainWindow.xaml.cs
@@ -14,11 +14,14 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CodeEditor.Views {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
         public MainWindow() {
+            Bootstrap.Initialize();
             InitializeComponent();
         }
     }
