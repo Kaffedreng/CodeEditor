3624100572c1b0c1cfd6d34930b3765446320b8e
Kaffedreng <kaffedreng@gmail.com>
2015-11-16 13:52:19 +0100

    Bugfixes - PluginManager
    Trying to add plugin manager - can't make it open a new window

---
+++ Source/CodeEditor/ViewModels/PluginManagerVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeEditor.Plugins;
+using CodeEditor.Views;
+
+namespace CodeEditor.ViewModels
+{
+    
+    internal class PluginManagerVm
+    {
+        private PluginInfo _childViewModel;
+
+        public PluginManagerVm()
+        {
+            _childViewModel = new PluginInfo();
+        }
+
+        public void Show()
+        {
+            PluginManager view = new PluginManager()
+            {
+                DataContext = _childViewModel
+            };
+
+            _childViewModel.Info = " was updated in the database.";
+
+            view.ShowDialog();
+        }
+    }
+}
