2e697cf46f6aa51e5f6e91460ad2d49592b54d9f
Christian Mitteldorf <christian.km@me.com>
2015-11-17 09:43:41 +0100

    Changed MainWindow to adopt MVVM

--- Source/CodeEditor/App.xaml.cs
+++ Source/CodeEditor/App.xaml.cs
@@ -7,9 +7,22 @@ using System.Threading.Tasks;
 using System.Windows;
 
 namespace CodeEditor {
+
+    using CodeEditor.ViewModels;
+    using CodeEditor.Views;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+
+            var mainWindow = new MainWindow();
+            mainWindow.DataContext = new MainWindowViewModel();
+            mainWindow.Show();
+        }
+
     }
 }
---
+++ Source/CodeEditor/Models/ObservableObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CodeEditor.ViewModels {
+
+    using System.ComponentModel;
+
+    // TODO: Document
+    public class ObservableObject : INotifyPropertyChanged {
+
+        /// <summary>
+        /// Event used to handle property changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// This method is called whenever the property is changed.
+        /// </summary>
+        /// <param name="propertyName">The property that changed.</param>
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "") {
+
+            // Sets the PropertyChangedEventHandler variable to whatever property was changed
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            // If ´handler´ is not empty, call PropertyChangedEventArgs
+            if (handler != null) {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
