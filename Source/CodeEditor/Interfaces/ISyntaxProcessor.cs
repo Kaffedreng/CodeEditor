dc31ab03d031f29492ce9b5d4f8ca4f9da1cfff5
Christian Mitteldorf <christian.km@me.com>
2015-11-18 15:46:50 +0100

    Finished ISyntaxProcessor.
    This interface can be used for plugins providing custom Syntax Highlighting.

--- Source/CodeEditor/Interfaces/ISyntaxProcessor.cs
+++ Source/CodeEditor/Interfaces/ISyntaxProcessor.cs
@@ -1,12 +1,53 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Windows.Documents;
+using System.Windows.Media;
+using CodeEditor.SyntaxHighlighting;
 
 namespace CodeEditor.Interfaces {
 
+    public interface ISyntaxProcessorCustomization {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contentType"></param>
+        /// <returns></returns>
+        HashSet<string> CustomKeywordsForContentType(ContentType contentType);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contentType"></param>
+        /// <returns>The color used for formatting, or null if no custom color is defined.</returns>
+        Color? CustomFontColorForContentType(ContentType contentType);
+
+    }
+
     public interface ISyntaxProcessor {
 
+        /// <summary>
+        /// A regular expression that defines how to split the words.
+        /// </summary>
         Regex SplitWordsRegex { get; }
-        int GetWordTypeId(string word);
-        Inline FormatInlineForId(Inline inline, int id);
+
+        /// <summary>
+        /// Each word has a specific content type.
+        /// All elements are isolated in Inline text, and passed to the FormatInlineWithContentType-method.
+        /// This way you can have different colors for different types of keywords.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        ContentType ContentTypeForWord(string word);
+
+        /// <summary>
+        /// Applies formatting (color) to the text of the specified content type
+        /// </summary>
+        /// <param name="inline"></param>
+        /// <param name="contentType"></param>
+        /// <returns></returns>
+        Inline FormatInlineWithContentType(Inline inline, ContentType contentType);
+
     }
 }
---
+++ Source/CodeEditor/SyntaxHighlighting/ContentType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeEditor.SyntaxHighlighting {
+
+    public enum ContentType {
+        PlainText,
+        Comments,
+        DocumentationMarkup,
+        DocumentationMarkupKeywords,
+        Strings,
+        Characters,
+        Numbers,
+        Keywords,
+        PreprocessorStatements,
+        URLs,
+        Attributes,
+        ProjectClassNames,
+        ProjectFunctionAndMethodNames,
+        ProjectConstants,
+        ProjectTypeNames,
+        ProjectInstanceVariablesAndGlobals,
+        ProjectPreprocessorMacros,
+        OtherClassNames,
+        OtherFunctionAndMethodNames,
+        OtherConstants,
+        OtherTypeNames,
+        OtherInstanceVariablesAndGlobals,
+        OtherPreprocessorMacros,
+    }
+}
