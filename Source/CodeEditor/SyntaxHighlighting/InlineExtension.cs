7ae94ed33c789f8bce378ae0fd490c9a12025442
Christian Mitteldorf <christian.km@me.com>
2015-11-18 15:57:15 +0100

    Added extension for Inline

---
+++ Source/CodeEditor/SyntaxHighlighting/InlineExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace CodeEditor.SyntaxHighlighting {
+
+    public static class InlineExtension {
+
+        public static Paragraph GetParagraph(this Inline inline) {
+
+            var contentElement = ((ContentElement)inline.Parent);
+
+            while (contentElement != null) {
+                var p = contentElement as Paragraph;
+
+                if (p != null) {
+                    return p;
+                }
+                contentElement = ((ContentElement)inline.Parent);
+            }
+
+            return null;
+        }
+    }
+}
