3a27a590345c86ee5acf8ec66f89979f109136d4
Christian Mitteldorf <christian.km@me.com>
2015-11-19 09:15:31 +0100

    Added event handler for TextEditorControl

--- Source/CodeEditor/Views/MainWindow.xaml.cs
+++ Source/CodeEditor/Views/MainWindow.xaml.cs
@@ -20,9 +20,21 @@ namespace CodeEditor.Views {
     /// </summary>
     public partial class MainWindow : Window {
 
+        private readonly TextEditorControl _textEditorControl;
+
         public MainWindow() {
-            Bootstrap.Initialize();
             InitializeComponent();
+
+            _textEditorControl = new TextEditorControl();
+
+            // Initialize Plugins, etc.
+            Bootstrap.Initialize();
+        }
+
+        private void EditorTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            if (_textEditorControl != null) {
+                _textEditorControl.TextDidChange(sender, e);
+            }
         }
     }
 }
