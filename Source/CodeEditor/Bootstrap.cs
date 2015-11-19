7f58bd0a040adac906af1eca7c0be2bbebb9750a
Christian Mitteldorf <christian.km@me.com>
2015-11-19 13:59:32 +0100

    Refactoring

--- Source/CodeEditor/Bootstrap.cs
+++ Source/CodeEditor/Bootstrap.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Windows;
 using CodeEditor.Plugins;
 
 namespace CodeEditor {
@@ -9,32 +8,23 @@ namespace CodeEditor {
         public static Dictionary<string, IPlugin> plugins = new Dictionary<string, IPlugin>();
 
         /// <summary>
-        /// Collects plugins, if there is any, and initializes them.
+        /// Collects plugins, if there are any, and initializes them.
         /// </summary>
         public static void Initialize() {
 
-            // Check for updates
-
             // Load Plug-Ins
-            var loadedPlugins = PluginLoader<IPlugin>.LoadPlugins("Plugins");
-            if (loadedPlugins != null)
-            {
-                foreach (var item in loadedPlugins)
-                {
+            var loadedPlugins = PluginLoader<IPlugin>.LoadPlugins("\\Plugins");
+            if (loadedPlugins != null) {
+                foreach (var item in loadedPlugins) {
                     plugins.Add(item.Name, item);
                 }
 
                 // Initialize Plug-Ins
-                foreach (var item in plugins)
-                {
+                foreach (var item in plugins) {
                     IPlugin plugin = item.Value;
                     plugin.Initialize();
                     plugin.Do();
                 }
-            }
-            else
-            {
-                MessageBox.Show("No plugins was found.");
             }
         }
     }
